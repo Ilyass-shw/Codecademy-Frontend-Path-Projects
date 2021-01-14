2d6f416b738d29e8ad2d685ea8c39b8e5b94ecc6
ilyass shw <souwain.malak@gmail.com>
2021-01-14 20:37:53 +0100

    do step one

--- Event Handler Registration/main.js
+++ Event Handler Registration/main.js
@@ -2,6 +2,6 @@ let readMore = document.getElementById('read-more');
 let moreInfo = document.getElementById('more-info');
 
 // Write your code here:
-function showInfo()={
-  
+function showInfo(){
+    moreInfo.style.display = 'block'; 
 }
