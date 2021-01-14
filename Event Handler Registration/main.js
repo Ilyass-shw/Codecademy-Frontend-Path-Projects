01837bbc63ed0295efe69a63abaa4d303e905571
ilyass shw <souwain.malak@gmail.com>
2021-01-14 20:41:02 +0100

    do step two

--- Event Handler Registration/main.js
+++ Event Handler Registration/main.js
@@ -4,4 +4,5 @@ let moreInfo = document.getElementById('more-info');
 // Write your code here:
 function showInfo(){
     moreInfo.style.display = 'block'; 
+    readMore.addEventListener('click', showInfo) ;
 }
