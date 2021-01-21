5d732c48f2528af4d84ed730603e44f2b40329a3
ilyass shw <souwain.malak@gmail.com>
2021-01-21 17:28:00 +0100

    add instance of primary school and use quickFacts on it

--- School Catalogue/main.js
+++ School Catalogue/main.js
@@ -23,13 +23,14 @@ class School {
     quickFacts(){
         console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`)
     }
-    pickSubstituteTeacher(substituteTeachers){
+    static pickSubstituteTeacher(substituteTeachers){
         return substituteTeachers[Math.floor(Math.random()*(substituteTeachers.length-1))]
     }
 }
 
 class Primary extends School{
-    constructor(pickupPolicy){
+    constructor(name, numberOfStudents, pickupPolicy){
+        super(name, 'primary', numberOfStudents);
         this._pickupPolicy = pickupPolicy 
     }
     get pickupPolicy(){
@@ -37,10 +38,15 @@ class Primary extends School{
     } 
 }
 
-class Middle extends School{}
+class Middle extends School{
+    constructor(name, numberOfStudents){
+        super(name, 'middle', numberOfStudents);
+    }
+}
 
 class High extends School{
-    constructor(sportsTeams){
+    constructor(name, level, numberOfStudents, sportsTeams){
+        super(name, 'high', numberOfStudents);
         this._sportsTeams = sportsTeams 
     }
     get sportsTeams(){
@@ -48,3 +54,7 @@ class High extends School{
     } 
 }
 
+
+const lorraineHansbury = new Primary('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+jhj
