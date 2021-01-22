933146474293ea56a3327930ce317b156bc8b2ca
ilyass shw <souwain.malak@gmail.com>
2021-01-22 16:20:05 +0100

    do step from 14

--- Message Mixer/message.js
+++ Message Mixer/message.js
@@ -10,7 +10,6 @@ function displayMessage() {
     console.log(MessageMixer.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
     console.log(MessageMixer.encode("What is the color of the sky?"));
     console.log(MessageMixer.palindrome("What is the color of the sky?"));
-    console.log(MessageMixer.pigLatin("What is the color of the sky?"));
   }
   
   displayMessage();
