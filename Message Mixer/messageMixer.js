0281ff1ad033e1923a9e88d44a89107cd19a23ff
ilyass shw <souwain.malak@gmail.com>
2021-01-22 15:50:21 +0100

    do steps from 1 to 4

--- Message Mixer/messageMixer.js
+++ Message Mixer/messageMixer.js
@@ -51,13 +51,53 @@ function countCharacter(inputString, inputCharacter) {
   };
   
   
-  function displayMessage() {
-    console.log(countCharacter("What is the color of the sky?", "t"));
-    console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-    console.log(reverseWord("What is the color of the sky?"));
-    console.log(reverseAllWords("What is the color of the sky?"));
-    console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-    console.log(encode("What is the color of the sky?"));
-  }
-  
-  displayMessage();
+const MessageMixer = {};
+
+MessageMixer.countCharacter = (inputString, inputCharacter) {
+    let count = 0;
+    let string = inputString.toLowerCase();
+    let character = inputCharacter.toLowerCase();
+      for (let i = 0; i < string.length; i++) {
+        if (string[i] === character) {
+           count++;
+        }
+      }
+    return count; 
+  };
+
+MessageMixer.capitalizeFirstCharacterOfWords = function(string) {
+    let arr = string.split(" ");  
+      for (let i = 0; i < arr.length; i++) {  
+        let word = arr[i];
+          arr[i] = word[0].toUpperCase() + word.substring(1); 
+      }
+    return arr.join(" "); 
+  };
+
+  MessageMixer.reverseWord = (word) {
+    return word.split("").reverse().join("");
+  };
+
+  MessageMixer.reverseAllWords = (sentence) {
+    let words = sentence.split(" ");
+      for (let i = 0; i < words.length; i++) {
+        words[i] = reverseWord(words[i]);
+      }
+     return words.join(" ");
+  };
+
+  MessageMixer.replaceFirstOccurence = (string, toBeReplaced, replaceWith) {
+    return string.replace(toBeReplaced, replaceWith);
+  };
+
+MessageMixer.replaceAllOccurrences = (string, toBeReplaced, replaceWith) {
+    return string.split(toBeReplaced).join(replaceWith);
+  };  
+
+  MessageMixer.encode = (string) {
+    let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
+      for (let key in replacementObject) {
+        string = replaceAllOccurrences(string, key, replacementObject[key]); 
+      }	
+      return string;
+  };
