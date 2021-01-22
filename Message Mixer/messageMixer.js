4ba2047ce9a214851898f19d300b19216ed6fe87
ilyass shw <souwain.malak@gmail.com>
2021-01-22 16:12:46 +0100

    do steps from 5 to 10

--- Message Mixer/messageMixer.js
+++ Message Mixer/messageMixer.js
@@ -1,59 +1,7 @@
-function countCharacter(inputString, inputCharacter) {
-    let count = 0;
-    let string = inputString.toLowerCase();
-    let character = inputCharacter.toLowerCase();
-      for (let i = 0; i < string.length; i++) {
-        if (string[i] === character) {
-           count++;
-        }
-      }
-    return count; 
-  };
-  
-  function capitalizeFirstCharacterOfWords(string) {
-    let arr = string.split(" ");  
-      for (let i = 0; i < arr.length; i++) {  
-        let word = arr[i];
-          arr[i] = word[0].toUpperCase() + word.substring(1); 
-      }
-    return arr.join(" "); 
-  };
-  
-  
-  function reverseWord(word) {
-    return word.split("").reverse().join("");
-  };
-  
-  function reverseAllWords(sentence) {
-    let words = sentence.split(" ");
-      for (let i = 0; i < words.length; i++) {
-        words[i] = reverseWord(words[i]);
-      }
-     return words.join(" ");
-  };
-  
-  
-  function replaceFirstOccurence(string, toBeReplaced, replaceWith) {
-    return string.replace(toBeReplaced, replaceWith);
-  };
-  
-  
-  function replaceAllOccurrences(string, toBeReplaced, replaceWith) {
-    return string.split(toBeReplaced).join(replaceWith);
-  };
-  
-  function encode(string) {
-    let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
-      for (let key in replacementObject) {
-        string = replaceAllOccurrences(string, key, replacementObject[key]); 
-      }	
-      return string;
-  };
-  
   
 const MessageMixer = {};
 
-MessageMixer.countCharacter = (inputString, inputCharacter) {
+MessageMixer.countCharacter = function (inputString, inputCharacter) {
     let count = 0;
     let string = inputString.toLowerCase();
     let character = inputCharacter.toLowerCase();
@@ -74,30 +22,40 @@ MessageMixer.capitalizeFirstCharacterOfWords = function(string) {
     return arr.join(" "); 
   };
 
-  MessageMixer.reverseWord = (word) {
+  MessageMixer.reverseWord = function(word) {
     return word.split("").reverse().join("");
   };
 
-  MessageMixer.reverseAllWords = (sentence) {
+  MessageMixer.reverseAllWords = function(sentence) {
     let words = sentence.split(" ");
       for (let i = 0; i < words.length; i++) {
-        words[i] = reverseWord(words[i]);
+        words[i] = MessageMixer.reverseWord(words[i]);
       }
      return words.join(" ");
   };
 
-  MessageMixer.replaceFirstOccurence = (string, toBeReplaced, replaceWith) {
+  MessageMixer.replaceFirstOccurence = function(string, toBeReplaced, replaceWith) {
     return string.replace(toBeReplaced, replaceWith);
   };
 
-MessageMixer.replaceAllOccurrences = (string, toBeReplaced, replaceWith) {
+MessageMixer.replaceAllOccurrences = function(string, toBeReplaced, replaceWith) {
     return string.split(toBeReplaced).join(replaceWith);
   };  
 
-  MessageMixer.encode = (string) {
+  MessageMixer.encode = function (string) {
     let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
       for (let key in replacementObject) {
-        string = replaceAllOccurrences(string, key, replacementObject[key]); 
+        string = MessageMixer.replaceAllOccurrences(string, key, replacementObject[key]); 
       }	
       return string;
   };
+
+  MessageMixer.palindrome = function (str){
+    return `${str} ${MessageMixer.reverseWord(str)}`;
+  }
+
+  MessageMixer.pigLatin = function (sentence, character){
+    return sentence.split(' ').join(character + ' ');
+  }
+
+  module.exports = MessageMixer;
