0abe5c2154cc4f9a0d4194fe0d8148ed9e5aa268
ilyass shw <souwain.malak@gmail.com>
2021-01-23 18:02:47 +0100

    do step

--- WorkAround/workaround.js
+++ WorkAround/workaround.js
@@ -1,11 +1,13 @@
 let Employee = require('./employee.js');
 function getEmployeeInformation(inputSalary) {
+
     Employee.salary = inputSalary;
     console.log('Cadre: ' + Employee.getCadre());
     console.log('Tax: ' + Employee.calculateTax());
     console.log('Benefits: ' + Employee.getBenefits());
     console.log('Bonus: ' + Employee.calculateBonus());
     console.log('Reimbursement Eligibility: ' + Employee.reimbursementEligibility() + '\n');
+    
   }
   
   getEmployeeInformation(10000);
