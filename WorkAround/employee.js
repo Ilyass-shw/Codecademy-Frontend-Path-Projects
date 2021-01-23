8b1031120e18eb96ee2ded542791dfe6819bf221
ilyass shw <souwain.malak@gmail.com>
2021-01-23 17:59:33 +0100

    do

--- WorkAround/employee.js
+++ WorkAround/employee.js
@@ -1,11 +1,13 @@
-let Employee = {};
-Employee.salary = 100000 ;
+
 
 let payGrades = {
   entryLevel: { taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999 },
   midLevel: { taxMultiplier: .1, benefits: ['health', 'housing'], minSalary: 50000, maxSalary: 99999 },
   seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
 };
+
+let Employee = {};
+Employee.salary = 100000 ;
 
 Employee.getCadre = function () {
   if (Employee.salary >= payGrades.entryLevel.minSalary && Employee.salary <= payGrades.entryLevel.maxSalary) {
