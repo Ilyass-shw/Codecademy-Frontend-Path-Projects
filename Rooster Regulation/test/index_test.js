e0c119e2a909548a65b2fe659d6a35680fe99f28
ilyass shw <souwain.malak@gmail.com>
2021-01-26 17:27:42 +0100

    do steps from 8 to 9

--- Rooster Regulation/test/index_test.js
+++ Rooster Regulation/test/index_test.js
@@ -12,8 +12,25 @@ describe('Rooster', ()=>{
             const actualCall = Rooster.announceDawn();
  
             // Use an assert method to compare actual and expected result
+            assert.equal(actualCall, expected);
+
+        })
+    })
+        
+    describe('.timeAtDawn', ()=>{
+        it('returns its argument as a string', ()=>{
+            // Define expected output
+            const expected = '20';
+            
+ 
+            // Call Rooster.announceDawn and store result in variable
+            const actualCall = Rooster.timeAtDawn(20);
+ 
+            // Use an assert method to compare actual and expected result
             assert.strictEqual(actualCall, expected);
 
         })
     })
+
+
 })
