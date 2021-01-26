59386e19f9a0b9bb0d22f3a2414c10ca3c57ee27
ilyass shw <souwain.malak@gmail.com>
2021-01-26 17:11:53 +0100

    do steps from 5 to 6

--- Rooster Regulation/index_test.js
+++ Rooster Regulation/index_test.js
@@ -3,6 +3,17 @@ let Rooster = require('./index..js');
 
 describe('Rooster', ()=>{
     describe('.annouceDawn', ()=>{
-        
+        it('returns a rooster call', ()=>{
+            // Define expected output
+            const expected = 'cock-a-doodle-doo!';
+            
+ 
+            // Call Rooster.announceDawn and store result in variable
+            const actualCall = Rooster.announceDawn();
+ 
+            // Use an assert method to compare actual and expected result
+            assert.strictEqual(actualCall, expected);
+
+        })
     })
 })
