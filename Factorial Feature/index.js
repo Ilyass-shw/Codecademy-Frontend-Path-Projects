655d72e664a63e859e75466245c4e3f1e8cb49b4
ilyass shw <souwain.malak@gmail.com>
2021-01-27 18:23:02 +0100

    do steps 9 and 10

--- Factorial Feature/index.js
+++ Factorial Feature/index.js
@@ -1,13 +1,18 @@
 const Calculate = {
   factorial(num){
-      let facNumbers = [];
-      for(i=0;i<=num-1;i++){
-          facNumbers.push(num-i)
-      };
-      const fact = facNumbers.reduce((acc, curr)=>{
-          return acc*curr;
-      });
-      return fact;
+      if( num === 0 || num === 1 ){
+          return 1;
+      }else{
+        let facNumbers = [];
+        for(i=0;i<=num-1;i++){
+            facNumbers.push(num-i)
+        };
+        const fact = facNumbers.reduce((acc, curr)=>{
+            return acc*curr;
+        });
+        return fact;
+      }
+
       }
   }
 
