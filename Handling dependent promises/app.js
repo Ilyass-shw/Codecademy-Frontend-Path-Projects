5a93b81fa807cf1477647499f6eb624d184be4b0
ilyass shw <souwain.malak@gmail.com>
2021-01-29 17:11:39 +0100

    do step 2

--- Handling dependent promises/app.js
+++ Handling dependent promises/app.js
@@ -2,5 +2,6 @@ const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
 
 // Write your code below:
 async function makeBeans(){
-    
+    const type = await shopForBeans();
+
 };
