6efaf27e7714a996690f736e00de05637bd4803e
ilyass shw <souwain.malak@gmail.com>
2021-01-31 13:23:49 +0100

    do steps 4 and 5 and 6

--- XHR POST Requests/main.js
+++ XHR POST Requests/main.js
@@ -1,3 +1,10 @@
 const xhr = new XMLHttpRequest();
 const url = "https://api-to-call.com/endpoint";
 const data = JSON.stringify({id : '200'});
+
+xhr.responseType = 'json';
+xhr.onreadystatechange= ()=>{
+    if(xhr.readyState === XMLHttpRequest.DONE){
+  
+    }
+  };
