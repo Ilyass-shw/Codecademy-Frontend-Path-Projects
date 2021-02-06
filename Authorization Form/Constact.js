baee3a23d199a8b66cdce38ef9ee20a377ea3409
ilyass shw <souwain.malak@gmail.com>
2021-02-06 17:28:55 +0100

    do step 6

--- Authorization Form/Constact.js
+++ Authorization Form/Constact.js
@@ -28,16 +28,19 @@ class Contact extends React.Component {
           <input
            type="submit" />
       </form>);
+      const contactInfo = (
+        <ul>
+        <li>
+          client@example.com
+        </li>
+        <li>
+          555.555.5555
+        </li>
+      </ul>
+      );
     return (
       <div id="authorization">
-            {this.state.authorized?<h1>Contact</h1> : <h1>Enter the Password</h1>}        <ul>
-          <li>
-            client@example.com
-          </li>
-          <li>
-            555.555.5555
-          </li>
-        </ul>
+            {this.state.authorized?<h1>Contact</h1> : <h1>Enter the Password</h1>}        
       </div>
     );
   }
