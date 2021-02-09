9a5bc16a88615caf06cf5fca01112565da2080f5
ilyass shw <souwain.malak@gmail.com>
2021-02-09 18:54:20 +0100

    do steps 8 and 9

--- Loading User Data/new-app/src/Components/Profile.js
+++ Loading User Data/new-app/src/Components/Profile.js
@@ -9,18 +9,6 @@ export class Profile extends React.Component {
   
   }
 
-  loadUserData(){
-
-    if(this.state.userData!=null){
-      this.setState({ userData: null })
-    };
-
-    this.fetchID = fetchUserData(this.props.username, (userData) => {
-      this.setState({ userData });
-    });
-
-  }
-
 
   render() {
     let isLoading;
@@ -44,7 +32,11 @@ export class Profile extends React.Component {
 
     return (
       <div className={className}>
-        <div className="profile-picture"></div>
+        <div className="profile-picture">
+        {!isLoading && (
+    <img src={this.state.userData.profilePictureUrl} alt="" />
+  )}
+        </div>
         <div className="profile-body">
           <h2>{name}</h2>
           <h3>@{this.props.username}</h3>
@@ -56,7 +48,30 @@ export class Profile extends React.Component {
     );
   }
 
+  loadUserData(){
+
+    if(this.state.userData!=null){
+      this.setState({ userData: null })
+    };
+
+    this.fetchID = fetchUserData(this.props.username, (userData) => {
+      this.setState({ userData });
+    });
+
+  }
+
+  componentDidUpdate(prevProps){
+    if(this.props.username !== prevProps.username){
+      cancelFetch(this.fetchID);
+      this.loadUserData();
+    }
+  }
+
   componentDidMount(){
     this.loadUserData()
+  }
+
+  componentWillUnmount(){
+    cancelFetch(this.fetchID);
   }
 }
