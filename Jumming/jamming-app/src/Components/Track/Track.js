d98cfe4feb4a1bad6e926f4bdfe79bff55262126
ilyass shw <souwain.malak@gmail.com>
2021-02-10 19:43:13 +0100

    fix bugs and make app render normally

--- Jumming/jamming-app/src/Components/Track/Track.js
+++ Jumming/jamming-app/src/Components/Track/Track.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import './Track.css'
 
-export class Track  extends React.component{
+export class Track  extends React.Component{
   constructor(props){
     super(props);
     this.props = {isRemoval : true};
   }
   renderAction(){
-    return isRemoval?<button className="Track-action">-</button> : <button className="Track-action">+</button>
+    return this.props.isRemoval?<button className="Track-action">-</button> : <button className="Track-action">+</button>
 
   }
     render(){
+      return (
 <div className="Track">
   <div className="Track-information">
-    <h3><!-- track name will go here --></h3>
-    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
-    {this.renderAction()}
+    <h3>!-- track name will go here --</h3>
+    <p>!-- track artist will go here-- | !-- track album will go here --</p>
   </div>
-  <button className="Track-action"><!-- + or - will go here --></button>
+  {this.renderAction()}
 </div>
+      );
     }
 }
