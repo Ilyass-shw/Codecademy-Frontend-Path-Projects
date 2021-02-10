24c919c6516d8b9c871754151021be7037f3e1fb
ilyass shw <souwain.malak@gmail.com>
2021-02-10 20:23:07 +0100

    do step 32

--- Jumming/jamming-app/src/Components/App/App.js
+++ Jumming/jamming-app/src/Components/App/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
   <div className="App">
     <SearchBar />
     <div className="App-playlist">  
-      <SearchResults />
+      <SearchResults searchResults={this.state.searchResults}/>
       <Playlist />
     </div>
   </div>
