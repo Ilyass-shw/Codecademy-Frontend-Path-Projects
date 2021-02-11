e035c4edd2198c9b9a2a203f85199c2ecc9dc000
ilyass shw <souwain.malak@gmail.com>
2021-02-11 17:46:10 +0100

    do steps 38 and 39

--- Jumming/jamming-app/src/Components/TrackList/TrackList.js
+++ Jumming/jamming-app/src/Components/TrackList/TrackList.js
@@ -4,12 +4,11 @@ import {Track} from '../Track/Track'
 export class TrackList extends React.Component{
     
     render(){
-        const Tracks = this.props.tracks.map(element=>{
-            return <Track name={element.name} album={element.album} artist={element.artist} key={element.id} />;
-        });
         return (
 <div className="TrackList">
-    {Tracks}
+    {this.props.tracks.map(element=>{
+            return <Track name={element.name} album={element.album} artist={element.artist} key={element.id} />;
+        })}
 </div>
         );
     }
