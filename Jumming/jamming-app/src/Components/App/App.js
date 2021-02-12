ae07f33817b9f072b859dcbebde84f8a204cdbd6
ilyass shw <souwain.malak@gmail.com>
2021-02-12 17:02:45 +0100

    do steps 68 and 69

--- Jumming/jamming-app/src/Components/App/App.js
+++ Jumming/jamming-app/src/Components/App/App.js
@@ -61,7 +61,7 @@ class App extends React.Component {
       <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-    <SearchBar />
+    <SearchBar onSearch={this.search}/>
     <div className="App-playlist">  
       <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
       <Playlist onSave = {this.savePlaylist} onNameChange= {this.updatePlaylistName}  playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
