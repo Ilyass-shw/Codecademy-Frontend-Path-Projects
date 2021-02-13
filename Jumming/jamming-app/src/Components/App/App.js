8328b8f841d6fd0e928fee703daa3f6c23a531a3
ilyass shw <souwain.malak@gmail.com>
2021-02-13 20:48:57 +0100

    do step 95

--- Jumming/jamming-app/src/Components/App/App.js
+++ Jumming/jamming-app/src/Components/App/App.js
@@ -16,21 +16,19 @@ class App extends React.Component {
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.search = this.search.bind(this);
 
-    this.state = {searchResults: [
-    {name: 'name1', artist: 'artist1', album: 'album1', id: '1'},
-    {name: 'name2', artist: 'artist2', album: 'album2', id: '2'},
-    {name: 'name3', artist: 'artist3', album: 'album3', id: '3'}],
-
+    this.state = {searchResults: [],
     playlistName: 'playlistName',
-    playlistTracks: [
-      {name: 'name1', artist: 'artist1', album: 'album1', id: '4'},
-      {name: 'name2', artist: 'artist2', album: 'album2', id: '5'},
-      {name: 'name3', artist: 'artist3', album: 'album3', id: '6'}] };
+    playlistTracks: [] };
   }
 
   savePlaylist(){
     const trackURIs = this.props.playlistName.map(element=> element.uri);
-    Spotify.savePlaylist(this.state.playlistName, trackURIs)
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(()=>{
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      })
+    });
   }
 
   updatePlaylistName(name){
