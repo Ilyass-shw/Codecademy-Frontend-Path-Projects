964d1310baf86dff1f29204c6ab32e5c44d83057
ilyass shw <souwain.malak@gmail.com>
2021-02-13 17:54:38 +0100

    do steps 81 to 87

--- Jumming/jamming-app/src/Util/Spotify.js
+++ Jumming/jamming-app/src/Util/Spotify.js
@@ -1,6 +1,7 @@
-import React from 'react';
-
 let userAccessToken;
+const clientId = d78b99e781f14bfdb6148b282c4dd921;
+const redirectedUri = "http://localhost:3000/";
+
 
 const Spotify = {
     getAccessToken(){
@@ -14,7 +15,10 @@ const Spotify = {
             userAccessToken = accessTokenMatch[1];
             const expiresIn = Number(expiresInMatch[1]);
             window.setTimeout(()=>{userAccessToken=''},  expiresIn * 1000);
-            window.history.pushState('Access Token', null, '/'); 
+            window.history.pushState('Access Token', null, '/');  
+            return userAccessToken;
+        }else{
+            window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectedUri}`
         }
 
     }
