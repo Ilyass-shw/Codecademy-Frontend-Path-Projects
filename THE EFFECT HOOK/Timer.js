4f45bbfb40860b03f8d5f05b0dc32633a81e5e9c
ilyass shw <souwain.malak@gmail.com>
2021-02-18 19:52:17 +0100

    do step 2

--- THE EFFECT HOOK/Timer.js
+++ THE EFFECT HOOK/Timer.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 export default function Timer() {
   const [time, setTime] = useState(0);
-
+  useEffect(()=>{
+    setInterval(()=>{setTime(prev=>prev+1)}, 1000);
+})
   return (
     <>
       <h1>Time: {time}</h1>
