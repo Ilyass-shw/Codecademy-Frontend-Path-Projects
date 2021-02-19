48667024048ad3a67feda50b350292a0e6207058
ilyass shw <souwain.malak@gmail.com>
2021-02-19 18:53:44 +0100

    do step 4

--- Passing Throughs/passing-through/src/AddThoughtForm.js
+++ Passing Throughs/passing-through/src/AddThoughtForm.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { generateId, getNewExpirationTime } from './utilities';
 
 export function AddThoughtForm(props) {
+
+  const [text, setText]= useState(['']);
+  const handleTextChange= ({target})=>{
+    setText(target.value);
+  }
+
   return (
     <form className="AddThoughtForm">
       <input
+        value={text}
         type="text"
         aria-label="What's on your mind?"
         placeholder="What's on your mind?"
+        onChange={handleTextChange}
       />
       <input type="submit" value="Add" />
     </form>
