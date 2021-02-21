14c200f12eb88ec5057f9eceeeeb39814bf1105f
ilyass shw <souwain.malak@gmail.com>
2021-02-21 20:40:20 +0100

    do step 3

--- counter-app/counter-app/src/App.test.js
+++ counter-app/counter-app/src/App.test.js
@@ -1,15 +1,11 @@
 import React from 'react';
-    import ReactDOM from 'react-dom';
+    import { shallow } from 'enzyme';
     import App from './App';
 
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<App />, div);
-      ReactDOM.unmountComponentAtNode(div);
-    });
-
-    describe('Addition', () => {
-      it('knows that 2 and 2 make 4', () => {
-        expect(2 + 2).toBe(4);
+    describe('App component', () => {
+      it('starts with a count of 0', () => {
+        const wrapper = shallow(<App />);
+        const text = wrapper.find('p').text();
+        expect(text).toEqual('Count: 0');
       });
     });
