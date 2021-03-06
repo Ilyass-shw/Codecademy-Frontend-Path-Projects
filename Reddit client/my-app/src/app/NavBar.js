43d2a2366472e64556ecbce2d4b7414ae4b35708
ilyass shw <souwain.malak@gmail.com>
2021-03-06 14:57:00 +0100

    seperate and progress css for NavBar

--- Reddit client/my-app/src/app/NavBar.js
+++ Reddit client/my-app/src/app/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./NavBar.css";
 import { Icon, InlineIcon } from "@iconify/react";
 import redditFill from "@iconify-icons/akar-icons/reddit-fill";
 import bxMenu from "@iconify-icons/bx/bx-menu";
@@ -23,38 +24,50 @@ import orderedList from "@iconify-icons/jam/ordered-list";
 import videoCamera from "@iconify-icons/majesticons/video-camera";
 
 // npm install --save-dev @iconify/react @iconify-icons/ic
-import baselineVideoLibrary from '@iconify-icons/ic/baseline-video-library';
+import baselineVideoLibrary from "@iconify-icons/ic/baseline-video-library";
 
+// npm install --save-dev @iconify/react @iconify-icons/ic
+// import baselineVideoLibrary from "@iconify-icons/ic/baseline-video-library";
 
 const NavBar = (props) => {
 	return (
-		<nav>
+		<div className="sideNav">
 			<ul>
 				{/* <li>
 					<Icon icon={redditFill} color="#FF0000" />
 				</li> */}
 				{/* <Icon icon={alienshipIcon} color="blue" /> */}
-				<li>
-					<Icon icon={bxMenu} color={"white"} width="1.5rem" height="1.5rem" />
-				</li>
+				<a href="#">
+					<li>
+						<Icon icon={bxMenu} color={"white"} width="1.5rem" height="1.5rem" />
+					</li>
+				</a>
 				{/* <Icon icon={bxTrendingUp} /> */}
-				<li>
+
+				<a href="#">
 					<Icon icon={homeIcon} color={"white"} width="1.2rem" height="1.2rem" />
-					Home
-				</li>
+					<li>Home</li>
+				</a>
+
 				{/* <Icon icon={graphBar} /> */}
-				<li>
+				<a href="#">
 					<Icon icon={sharpLocalFireDepartment} color={"white"} width="1.2rem" height="1.2rem" />
-                    Trending
-				</li>
+					<li>Trending</li>
+				</a>
+
 				{/* <li><Icon icon={orderedList} /></li> */}
-				<li>
-					<Icon icon={baselineSubscriptions} color={"white"} width="1.2rem" height="1.2rem" />
-                    subscriptions
-				</li>
+				<a href="#">
+					<Icon icon={baselineSubscriptions} color={"white"} width="1.2rem" height="1.2rem" className="menu-icon" />
+					<li>Subscriptions</li>
+				</a>
+
 				{/* <li><Icon icon={baselineSubscriptions} /></li> */}
+				<a href="#">
+					<Icon icon={baselineVideoLibrary} color={"white"} width="1.5rem" height="1.5rem" className="menu-icon" />
+					<li>Library</li>
+				</a>
 			</ul>
-		</nav>
+		</div>
 	);
 };
 
