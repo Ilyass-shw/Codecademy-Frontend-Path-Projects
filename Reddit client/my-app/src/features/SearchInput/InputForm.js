669e2c90bbe3b4f000c2157949df3fb2901d90fd
ilyass shw <souwain.malak@gmail.com>
2021-03-07 14:34:53 +0100

    add search-button with its styling

--- Reddit client/my-app/src/features/SearchInput/InputForm.js
+++ Reddit client/my-app/src/features/SearchInput/InputForm.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import "./InputForm.css";
+// npm install --save-dev @iconify/react @iconify-icons/fluent
+import { Icon, InlineIcon } from '@iconify/react';
+import search16Filled from '@iconify-icons/fluent/search-16-filled';
+
 
 export const InputForm = () => {
 	const [term, setTerm] = useState("");
@@ -9,8 +13,9 @@ export const InputForm = () => {
 	};
 	return (
 		<div className="input">
-			<form action='#'>
+			<form action='#' className='input-form'>
 				<input type="text" name="search" value={term} onChange={handleChange} placeholder='Search' />
+				<button className='search-button'><InlineIcon icon={search16Filled} width="1.22rem" height="1.22rem" color={'#6f6f6f'}/></button>
 			</form>
 		</div>
 	);
