f352abd577b5e40195956ab3b3bfdabb010e2581
ilyass shw <souwain.malak@gmail.com>
2021-03-08 16:16:32 +0100

    create Post.js component

--- Reddit client/my-app/src/features/Posts/Post.js
+++ Reddit client/my-app/src/features/Posts/Post.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Post = props => {
+const Post = ({post}) => {
     return (
         <div>
-            
+            <img src={post.img} alt='post'/>
+            <h3>{post.title}</h3>
+            <p>{post.upvotes}</p>
+            <p>{post.date}</p>
+            <img src={post.subredditIcon} alt="subreddit's icon"/>
+            <h4>{post.subreddit}</h4>
         </div>
     )
 }
 
 Post.propTypes = {
-
+post: PropTypes.object.isRequired,
+key: PropTypes.number.isRequired
 }
 
 export default Post
