f7e0c585f3cda6193838e7c2dcd07c5f84c51503
ilyass shw <souwain.malak@gmail.com>
2021-03-08 20:15:22 +0100

    add more style into post.css

--- Reddit client/my-app/src/features/Posts/Post.js
+++ Reddit client/my-app/src/features/Posts/Post.js
@@ -9,12 +9,16 @@ const Post = ({ post }) => {
 		<div className="post-block">
 			<img src={im} alt="post" className="content-image" />
 			<div className="post-data">
-				<div className='post-data-container'>
+				<div className="post-data-container">
 					<h4 className="title">{post.title}</h4>
-					<p>{post.upvotes}</p>
-					<p>{post.date}</p>
-					<img src={logo} alt="subreddit's icon" className="subreddit-icon" />
-					<p>{post.subreddit}</p>
+					<div className='upvotes-date-data'>
+						<p>{post.upvotes} upvotes<span className='dot'>•</span></p>
+						<p>{post.date}</p>
+					</div>
+					<div className='subreddit'>
+						<img src={logo} alt="subreddit's icon" className="subreddit-icon" />
+						<p>{post.subreddit}</p>
+					</div>
 				</div>
 			</div>
 		</div>
