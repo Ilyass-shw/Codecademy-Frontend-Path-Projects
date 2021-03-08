10657fd971dcae836f10a1b8b74d1c3de0f0d7a4
ilyass shw <souwain.malak@gmail.com>
2021-03-08 13:52:59 +0100

    create fake posts data inside initialState in postsSlice.js

--- Reddit client/my-app/src/features/Posts/PostsSlice.js
+++ Reddit client/my-app/src/features/Posts/PostsSlice.js
@@ -1,12 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+	posts: [
+		{
+			title: "title1",
+			img: "src1",
+			upvotes: 10,
+			date: "1 min ago 1",
+			author: "arthur1",
+			subreddit: "r/German1",
+			subredditIcon: "src1",
+			content: "text1",
+		},
+		{
+			title: "title2",
+			img: "src2",
+			upvotes: 10,
+			date: "1 min ago 2",
+			author: "arthur 2",
+			subreddit: "r/German 2",
+			subredditIcon: "src 2",
+			content: "text 2",
+		},
+		{
+			title: "title3",
+			img: "src3",
+			upvotes: 10,
+			date: "1 min ago 3",
+			author: "arthur 3",
+			subreddit: "r/German 3",
+			subredditIcon: "src 3",
+			content: "text 3",
+		},
+	],
+};
 
-const PostsSlice = createSlice({
-	name: "Posts",
+const postsSlice = createSlice({
+	name: "posts",
 	initialState,
 	reducers: {},
 });
 
-export const {} = PostsSlice.actions;
-export default PostsSlice.reducer;
+export const {} = postsSlice.actions;
+export default postsSlice.reducer;
+
+
