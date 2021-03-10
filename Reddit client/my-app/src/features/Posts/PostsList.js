79d26608d5721cf7df3d6a65a1c768b577798c65
ilyass shw <souwain.malak@gmail.com>
2021-03-10 21:14:17 +0100

    create extraReducers to handle fetchedPosts action postsSlice

--- Reddit client/my-app/src/features/Posts/PostsList.js
+++ Reddit client/my-app/src/features/Posts/PostsList.js
@@ -5,22 +5,30 @@ import "./PostsList.css";
 import { postAdded, fetchPosts, selectAllPosts } from "./postsSlice";
 
 const PostsList = () => {
+	const postStatus = useSelector((state) => state.posts.status);
 
 	const dispatch = useDispatch();
-	useEffect(()=>{
-		dispatch(fetchPosts());
-		dispatch(postAdded([{
-			title: "title2",
-			img: "./imgs/Untitled.png",
-			upvotes: 100,
-			date: "1 min ago 2",
-			author: "arthur 2",
-			subreddit: "r/German 2",
-			subredditIcon: "./imgs/icons/favicon.ico",
-			content: "text 2",
-			id: 2,
-		}]));
-   },[dispatch])
+	useEffect(() => {
+		if (postStatus === "idle") {
+			dispatch(fetchPosts());
+		}
+
+		dispatch(
+			postAdded([
+				{
+					title: "title2",
+					img: "./imgs/Untitled.png",
+					upvotes: 100,
+					date: "1 min ago 2",
+					author: "arthur 2",
+					subreddit: "r/German 2",
+					subredditIcon: "./imgs/icons/favicon.ico",
+					content: "text 2",
+					id: 2,
+				},
+			])
+		);
+	}, [postStatus, dispatch]);
 
 	const posts = useSelector(selectAllPosts);
 	const content = posts.map((post) => {
@@ -33,5 +41,4 @@ const PostsList = () => {
 	);
 };
 
- 
 export default PostsList;
--- Reddit client/my-app/src/features/Posts/PostsSlice.js
+++ Reddit client/my-app/src/features/Posts/PostsSlice.js
@@ -5,6 +5,34 @@ import { formatDistanceToNow } from "date-fns";
 import { useDispatch } from "react-redux";
 import millify from "millify";
 
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+	const response = await client.get(
+		"https://www.reddit.com/search.json?q=puppies&include_facets=true&limit=2&restrict_sr=true&sort=relevance&t=all"
+	);
+	console.log(response);
+
+	const fetchedPosts = response.data.children.map((post) => {
+		let postDate = fromUnixTime(post.data.created_utc);
+		postDate = formatDistanceToNow(postDate, { addSuffix: true, includeSeconds: true });
+		const upvotes = millify(post.data.ups);
+
+		return {
+			title: post.data.title,
+			img: post.data.rpan_video.hls_url,
+			upvotes,
+			date: postDate,
+			author: post.data.author,
+			subreddit: post.data.subreddit_name_prefixed,
+			subredditIcon: post.data.rpan_video.scrubber_media_url,
+			content: "text1",
+			id: post.data.id,
+		};
+	});
+
+	console.log(fetchedPosts);
+	return fetchedPosts;
+});
+
 const initialState = {
 	posts: [
 		{
@@ -41,6 +69,8 @@ const initialState = {
 			id: 3,
 		},
 	],
+	status: "idle",
+	error: null,
 };
 
 const postsSlice = createSlice({
@@ -48,40 +78,27 @@ const postsSlice = createSlice({
 	initialState,
 	reducers: {
 		postAdded(state, action) {
-			state.posts.push(action.payload)
-		  },
+			state.posts.push(action.payload);
+		},
+	},
+	extraReducers: {
+		[fetchPosts.pending]: (state, action) => {
+			state.status = "Loading";
+		},
+		[fetchPosts.fulfilled]: (state, action) => {
+			state.status = "succeeded";
+			state.posts = state.posts.concat(action.payload);
+		},
+		[fetchPosts.pending]: (state, action) => {
+			state.status = "failed";
+			state.error = action.error.message;
+		},
 	},
 });
 
 export const { postAdded } = postsSlice.actions;
 export default postsSlice.reducer;
 
-export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
-	const response = await client.get(
-		"https://www.reddit.com/search.json?q=puppies&include_facets=true&limit=2&restrict_sr=true&sort=relevance&t=all"
-	);
-	console.log(response);
 
-	const fetchedPosts = response.data.children.map((post) => {
-		let postDate = fromUnixTime(post.data.created_utc);
-		postDate = formatDistanceToNow(postDate, { addSuffix: true, includeSeconds: true });
-		const upvotes = millify(post.data.ups);
-		
-		return {
-			title: post.data.title,
-			img: post.data.rpan_video.hls_url,
-			upvotes,
-			date: postDate,
-			author: post.data.author,
-			subreddit: post.data.subreddit_name_prefixed,
-			subredditIcon: post.data.rpan_video.scrubber_media_url,
-			content: "text1",
-			id: post.data.id,
-		};
-	});
-
-	console.log(fetchedPosts);
-	return fetchedPosts;
-});
 
 export const selectAllPosts = (state) => state.posts.posts;
