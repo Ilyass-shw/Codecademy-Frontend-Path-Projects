29a064495f5d58f3fe0c151e4be2635cdc40bafa
ilyass shw <souwain.malak@gmail.com>
2021-03-21 19:28:27 +0100

    fix position of skeleton for small devices

--- Reddit client/my-app/src/features/PostsSkeleton/PostsSkeleton.js
+++ Reddit client/my-app/src/features/PostsSkeleton/PostsSkeleton.js
@@ -6,18 +6,18 @@ const PostsSkeleton = () => {
 	return (
 		<div>
 			<SkeletonTheme color="#262626" highlightColor="#444">
-				<div className="post-block">
+				<div className="post-block skeleton-post-block">
 					<div className="content-image">
 						<Skeleton width="240px" height="10rem" />
 					</div>
 
-					<div className="post-data">
-						<div className="post-data-container">
+					<div className="post-data skeleton-post-data-mobile">
+						<div className="post-data-container skeleton-post-data-container-mobile">
 							<Skeleton width="180px" height="20px" />
 							<Skeleton width="140px" height="13px" />
 						</div>
 
-						<div className="subreddit">
+						<div className="subreddit skeleton-subreddit-mobile">
 							<Skeleton className="subreddit-icon" />
 							<Skeleton width="80px" height="13px" />
 						</div>
