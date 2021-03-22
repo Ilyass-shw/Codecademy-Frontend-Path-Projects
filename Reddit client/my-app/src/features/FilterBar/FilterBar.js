b64ff58d2510bb0ec70e0f0567a7555b543deb8f
ilyass shw <souwain.malak@gmail.com>
2021-03-22 16:01:06 +0100

    fix placement of arrows when filterBar is scrolled

--- Reddit client/my-app/src/features/FilterBar/FilterBar.js
+++ Reddit client/my-app/src/features/FilterBar/FilterBar.js
@@ -31,63 +31,65 @@ export const FilterBar = () => {
 		dispatch(filterUpdated(filterBy));
 		dispatch(fetchPosts());
 	};
-	const scrollLeft = () => document.getElementById("filter-bar").scrollLeft-= 100;
-	const scrollRight = () => document.getElementById("filter-bar").scrollLeft+= 100;
+	const scrollLeft = () => (document.getElementById("filter-bar").scrollLeft -= 100);
+	const scrollRight = () => (document.getElementById("filter-bar").scrollLeft += 100);
 
 	return (
-		<div className="filter-bar" id="filter-bar">
+		<div className="filter-bar-container">
+			<div className="filter-bar" id="filter-bar">
 				<div className="arrow left">
 					<div className="arrow-button" onClick={scrollLeft}>
 						<InlineIcon icon={arrowIosBackOutline} />
 					</div>
 				</div>
 
-			<button
-				className={`filter-button ${"relevance" === filterBy && "selected"}`}
-				id="relevance"
-				value="relevance"
-				onClick={handleOnClick}
-			>
-				<InlineIcon icon={bxsRocket} /> Relevance
-			</button>
-			<button
-				className={`filter-button ${"hot" === filterBy && "selected"}`}
-				id="hot"
-				value="hot"
-				onClick={handleOnClick}
-			>
-				<InlineIcon icon={fireFilled} /> Hot
-			</button>
-			<button
-				className={`filter-button ${"new" === filterBy && "selected"}`}
-				id="new"
-				value="new"
-				onClick={handleOnClick}
-			>
-				<InlineIcon icon={sharpNewReleases} /> New
-			</button>
-			<button
-				className={`filter-button ${"top" === filterBy && "selected"}`}
-				id="top"
-				value="top"
-				onClick={handleOnClick}
-			>
-				<InlineIcon icon={verticalAlignTop} width=".6rem" height=".6rem" /> Top
-			</button>
-			<button
-				className={`filter-button ${"comments" === filterBy && "selected"}`}
-				id="comments"
-				value="comments"
-				onClick={handleOnClick}
-			>
-				<InlineIcon icon={commentsIcon} /> Comments
-			</button>
+				<button
+					className={`filter-button ${"relevance" === filterBy && "selected"}`}
+					id="relevance"
+					value="relevance"
+					onClick={handleOnClick}
+				>
+					<InlineIcon icon={bxsRocket} /> Relevance
+				</button>
+				<button
+					className={`filter-button ${"hot" === filterBy && "selected"}`}
+					id="hot"
+					value="hot"
+					onClick={handleOnClick}
+				>
+					<InlineIcon icon={fireFilled} /> Hot
+				</button>
+				<button
+					className={`filter-button ${"new" === filterBy && "selected"}`}
+					id="new"
+					value="new"
+					onClick={handleOnClick}
+				>
+					<InlineIcon icon={sharpNewReleases} /> New
+				</button>
+				<button
+					className={`filter-button ${"top" === filterBy && "selected"}`}
+					id="top"
+					value="top"
+					onClick={handleOnClick}
+				>
+					<InlineIcon icon={verticalAlignTop} width=".6rem" height=".6rem" /> Top
+				</button>
+				<button
+					className={`filter-button ${"comments" === filterBy && "selected"}`}
+					id="comments"
+					value="comments"
+					onClick={handleOnClick}
+				>
+					<InlineIcon icon={commentsIcon} /> Comments
+				</button>
 
 				<div className="arrow right">
 					<div className="arrow-button" onClick={scrollRight}>
 						<InlineIcon icon={arrowIosForwardOutline} />
 					</div>
 				</div>
+			</div>
 		</div>
 	);
 };
