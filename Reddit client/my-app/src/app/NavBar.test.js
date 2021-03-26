a616dad0d400951609e9c2d76dd4b79830d5a70c
ilyass shw <souwain.malak@gmail.com>
2021-03-26 22:23:14 +0100

    update snapshot test for NavBar

--- Reddit client/my-app/src/app/NavBar.test.js
+++ Reddit client/my-app/src/app/NavBar.test.js
@@ -3,17 +3,14 @@ import { render } from "@testing-library/react";
 import React from "react";
 import NavBar from "./NavBar";
 
-jest.mock("@iconify-icons/bx/bx-menu", ()=>() =><div>mock</div>)
-jest.mock("@iconify-icons/entypo/home", ()=>() =><div>mock</div>)
-jest.mock("@iconify-icons/ic/sharp-local-fire-department", ()=>() =><div>mock</div>)
-jest.mock("@iconify-icons/ic/baseline-subscriptions", ()=>() =><div>mock</div>)
-jest.mock("@iconify-icons/ic/baseline-video-library", ()=>() =><div>mock</div>)
-describe('NavBar',()=>{
+jest.mock("@iconify-icons/bx/bx-menu", () => () => <div>mock</div>);
+jest.mock("@iconify-icons/entypo/home", () => () => <div>mock</div>);
+jest.mock("@iconify-icons/ic/sharp-local-fire-department", () => () => <div>mock</div>);
+jest.mock("@iconify-icons/ic/baseline-subscriptions", () => () => <div>mock</div>);
+jest.mock("@iconify-icons/ic/baseline-video-library", () => () => <div>mock</div>);
+describe("NavBar", () => {
 	it("should matche the snapshot", () => {
-
-		const component = render(<NavBar />);
-		// console.log(component)
-		// expect(container).toMatchSnapshot();
+		const { container } = render(<NavBar />);
+		expect(container).toMatchSnapshot();
 	});
-})
-
+});
