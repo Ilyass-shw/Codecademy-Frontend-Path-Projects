cb20e31914413a93e690e6a291007ac0c83e7f8f
ilyass shw <souwain.malak@gmail.com>
2021-04-02 21:59:28 +0100

    crop filterButtons in FilterBar and replace them in their own new component: FilterButtons.js

--- Reddit client/my-app/src/features/FilterBar/FilterBar.js
+++ Reddit client/my-app/src/features/FilterBar/FilterBar.js
@@ -1,32 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+
+import "./FilterBar.css";
+import { styleArrowsToDisplayNone, updateArrowStyle } from "./FilterBarUtils";
+import FilterButtons from "../FilterButtons/FilterButtons";
 
 import { InlineIcon } from "@iconify/react";
-import bxsRocket from "@iconify-icons/bx/bxs-rocket";
-import fireFilled from "@iconify-icons/ant-design/fire-filled";
-import sharpNewReleases from "@iconify-icons/ic/sharp-new-releases";
-import "./FilterBar.css";
-import verticalAlignTop from "@iconify/icons-oi/vertical-align-top";
-import commentsIcon from "@iconify/icons-icons8/comments";
+
 import arrowIosForwardOutline from "@iconify-icons/eva/arrow-ios-forward-outline";
 import arrowIosBackOutline from "@iconify-icons/eva/arrow-ios-back-outline";
 
-import { filterUpdated, fetchPosts } from "../posts/postsSlice";
-import { styleArrowsToDisplayNone, updateArrowStyle } from "./FilterBarUtils";
+
 
 export const FilterBar = () => {
-	const [filterBy, setFilterBy] = useState("relevance");
 
 	const [styleRightArrow, setStyleRightArrow] = useState({});
 	const [styleLeftArrow, setStyleLeftArrow] = useState({});
 
 	const [pixelsScrolledLeft, setPixelsScrolledLeft] = useState(0);
 
-	const dispatch = useDispatch();
 
-	const handleOnClick = ({ target }) => {
-		setFilterBy(target.value);
-	};
+	
 
 	const handleScrollLeft = () => {
 		document.getElementById("filter-bar").scrollLeft -= 100;
@@ -43,10 +36,7 @@ export const FilterBar = () => {
 
 		setPixelsScrolledLeft(pixelsScrolledLeft + 1);
 	};
-	useEffect(() => {
-		dispatch(filterUpdated(filterBy));
-		dispatch(fetchPosts());
-	}, [filterBy, dispatch]);
+	
 
 	useEffect(() => {
 		const timeOut = setTimeout(() => {
@@ -67,46 +57,7 @@ export const FilterBar = () => {
 					</div>
 				</div>
 
-				<button
-					className={`filter-button ${"relevance" === filterBy && "selected"}`}
-					id="relevance"
-					value="relevance"
-					onClick={handleOnClick}
-				>
-					<InlineIcon icon={bxsRocket} /> Relevance
-				</button>
-				<button
-					className={`filter-button ${"hot" === filterBy && "selected"}`}
-					id="hot"
-					value="hot"
-					onClick={handleOnClick}
-				>
-					<InlineIcon icon={fireFilled} /> Hot
-				</button>
-				<button
-					className={`filter-button ${"new" === filterBy && "selected"}`}
-					id="new"
-					value="new"
-					onClick={handleOnClick}
-				>
-					<InlineIcon icon={sharpNewReleases} /> New
-				</button>
-				<button
-					className={`filter-button ${"top" === filterBy && "selected"}`}
-					id="top"
-					value="top"
-					onClick={handleOnClick}
-				>
-					<InlineIcon icon={verticalAlignTop} width=".6rem" height=".6rem" /> Top
-				</button>
-				<button
-					className={`filter-button ${"comments" === filterBy && "selected"}`}
-					id="comments"
-					value="comments"
-					onClick={handleOnClick}
-				>
-					<InlineIcon icon={commentsIcon} /> Comments
-				</button>
+				<FilterButtons />
 
 				<div className="arrow right" style={styleRightArrow}>
 					<div className="arrow-button" onClick={handleScrollRight}>
---
+++ Reddit client/my-app/src/features/FilterButtons/FilterButtons.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { InlineIcon } from "@iconify/react";
+import bxsRocket from "@iconify-icons/bx/bxs-rocket";
+import fireFilled from "@iconify-icons/ant-design/fire-filled";
+import sharpNewReleases from "@iconify-icons/ic/sharp-new-releases";
+import verticalAlignTop from "@iconify/icons-oi/vertical-align-top";
+import commentsIcon from "@iconify/icons-icons8/comments";
+
+import { filterUpdated, fetchPosts } from "../posts/postsSlice";
+
+const FilterButtons = () => {
+	const [filterBy, setFilterBy] = useState("relevance");
+
+    const dispatch = useDispatch();
+
+	const handleOnClick = ({ target }) => {
+		setFilterBy(target.value);
+	};
+
+
+	useEffect(() => {
+		dispatch(filterUpdated(filterBy));
+		dispatch(fetchPosts());
+	}, [filterBy, dispatch]);
+	return (
+		<div>
+			<button
+				className={`filter-button ${"relevance" === filterBy && "selected"}`}
+				id="relevance"
+				value="relevance"
+				onClick={handleOnClick}
+			>
+				<InlineIcon icon={bxsRocket} /> Relevance
+			</button>
+			<button
+				className={`filter-button ${"hot" === filterBy && "selected"}`}
+				id="hot"
+				value="hot"
+				onClick={handleOnClick}
+			>
+				<InlineIcon icon={fireFilled} /> Hot
+			</button>
+			<button
+				className={`filter-button ${"new" === filterBy && "selected"}`}
+				id="new"
+				value="new"
+				onClick={handleOnClick}
+			>
+				<InlineIcon icon={sharpNewReleases} /> New
+			</button>
+			<button
+				className={`filter-button ${"top" === filterBy && "selected"}`}
+				id="top"
+				value="top"
+				onClick={handleOnClick}
+			>
+				<InlineIcon icon={verticalAlignTop} width=".6rem" height=".6rem" /> Top
+			</button>
+			<button
+				className={`filter-button ${"comments" === filterBy && "selected"}`}
+				id="comments"
+				value="comments"
+				onClick={handleOnClick}
+			>
+				<InlineIcon icon={commentsIcon} /> Comments
+			</button>
+		</div>
+	);
+};
+
+export default FilterButtons;
