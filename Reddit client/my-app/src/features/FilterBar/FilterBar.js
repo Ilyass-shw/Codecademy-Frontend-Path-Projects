cdd4ad1a8255a8712d5cbc5271f7547b290c70a4
ilyass shw <souwain.malak@gmail.com>
2021-04-03 13:21:29 +0100

    fix the resizing event listener functionality in FilterBar.js

--- Reddit client/my-app/src/features/FilterBar/FilterBar.js
+++ Reddit client/my-app/src/features/FilterBar/FilterBar.js
@@ -8,19 +8,22 @@ import { InlineIcon } from "@iconify/react";
 
 import arrowIosForwardOutline from "@iconify-icons/eva/arrow-ios-forward-outline";
 import arrowIosBackOutline from "@iconify-icons/eva/arrow-ios-back-outline";
+import useWindowDimensions from "../Helper/UseWindowDimensions";
 
 export const FilterBar = () => {
 	const [styleRightArrow, setStyleRightArrow] = useState({});
 	const [styleLeftArrow, setStyleLeftArrow] = useState({});
 
-	let [rerender, setRerender] = useState(0); // this state is created to trigger a new render
+	const { width } = useWindowDimensions(); // this includes resize event listener
+
+	let [rerender, setRerender] = useState(width); //this state is to trigger a new render when window.dimension changes
 
 	const handleScrollLeft = () => {
 		document.getElementById("filter-bar").scrollLeft -= 100;
 
-		setRerender(rerender + 1);
-
 		styleArrowsToDisplayNone(setStyleLeftArrow, setStyleRightArrow);
+
+		setRerender(rerender + width);
 	};
 
 	const handleScrollRight = () => {
@@ -28,43 +31,18 @@ export const FilterBar = () => {
 
 		styleArrowsToDisplayNone(setStyleLeftArrow, setStyleRightArrow);
 
-		setRerender(rerender + 1);
+		setRerender(rerender + width);
 	};
 
 	useEffect(() => {
 		const timeOut = setTimeout(() => {
 			updateArrowStyle(setStyleLeftArrow, setStyleRightArrow);
-		}, 100);
-
-		window.addEventListener("scroll", () => {
-			console.log("scrolled");
-			setRerender(rerender + 1);
-
-			updateArrowStyle(setStyleLeftArrow, setStyleRightArrow);
-		});
-		window.addEventListener("resize", () => {
-			console.log("resized");
-			setRerender(rerender + 1);
-
-			updateArrowStyle(setStyleLeftArrow, setStyleRightArrow);
-		});
+		}, 200);
 
 		return () => {
 			clearTimeout(timeOut);
-			window.removeEventListener("scroll", () => {
-				console.log("scrolled");
-				setRerender(rerender + 1);
-
-				updateArrowStyle(setStyleLeftArrow, setStyleRightArrow);
-			});
-			window.removeEventListener("resize", () => {
-				console.log("resized");
-				setRerender(rerender + 1);
-
-				updateArrowStyle(setStyleLeftArrow, setStyleRightArrow);
-			});
 		};
-	}, [setStyleLeftArrow, setStyleRightArrow, setRerender, rerender]);
+	}, [setStyleLeftArrow, setStyleRightArrow, rerender, width]);
 
 	return (
 		<div className="filter-bar-container">
