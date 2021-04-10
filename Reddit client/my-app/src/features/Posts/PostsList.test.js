2edb1ab0c4f3736a49b7705e1ae67ecb228557f2
ilyass shw <souwain.malak@gmail.com>
2021-04-10 20:20:46 +0100

    test PostSlist as it should display skeleton and match the snapshot when while posts fetching is not completed yet

--- Reddit client/my-app/src/features/Posts/PostsList.test.js
+++ Reddit client/my-app/src/features/Posts/PostsList.test.js
@@ -4,14 +4,13 @@ import { jest } from "@jest/globals";
 import { render, makeTestStore, fireEvent, screen } from "../testUtils/testUtils";
 import { fetchPosts } from "../posts/postsSlice";
 
-let store;
 describe("PostsSkeleton", () => {
 	it("should render first time (with posts.status = idle, in store) without crashing ", () => {
 		const initialState = {
 			status: "idle",
 		};
 		const name = "posts";
-		store = makeTestStore(name, initialState);
+		const store = makeTestStore(name, initialState);
 
 		render(<PostsList />, { store });
 	});
@@ -21,18 +20,14 @@ describe("PostsSkeleton", () => {
 			status: "idle",
 		};
 		const name = "posts";
-		store = makeTestStore(name, initialState);
-		console.log(fetchPosts())
-
-		jest.mock("./postsSlice", () => {
-			return { fetchPosts: () => {console.log('wiyéééé')} };
-		});
+		const store = makeTestStore(name, initialState);
+		console.log(fetchPosts());
 
 		render(<PostsList />, { store });
 
 		expect(store.dispatch).toHaveBeenCalledTimes(1);
-		// expect(fetchPosts).toHaveBeenCalledTimes(2);
-		// expect(store.dispatch).toHaveBeenCalledWith(fetchPosts());
+	// 	// expect(fetchPosts).toHaveBeenCalledTimes(2);
+	// 	// expect(store.dispatch).toHaveBeenCalledWith(fetchPosts());
 	});
 
 	it("should display skeleton and match the snapshot when while posts are not fetched yet ", () => {
@@ -40,19 +35,18 @@ describe("PostsSkeleton", () => {
 			status: "idle",
 		};
 		const name = "posts";
-		store = makeTestStore(name, initialState);
+		const store = makeTestStore(name, initialState);
 		const { container } = render(<PostsList />, { store });
 		expect(container).toMatchSnapshot();
 	});
 
-	// it("should display skeleton and match the snapshot when while posts fetching is not completed yet ", () => {
-	// 	const initialState = {
-	// 		status: "Loading",
-	// 	};
-	// 	const name = "posts";
-	// 	store = makeTestStore(name, initialState);
-	// 	const { container } = render(<PostsList />, { store });
-	// 	expect(container).toMatchSnapshot();
-	// });
-
+	it("should display skeleton and match the snapshot when while posts fetching is not completed yet ", () => {
+		const initialState = {
+			status: "Loading",
+		};
+		const name = "posts";
+		const store = makeTestStore(name, initialState);
+		const { container } = render(<PostsList />, { store });
+		expect(container).toMatchSnapshot();
+	});
 });
