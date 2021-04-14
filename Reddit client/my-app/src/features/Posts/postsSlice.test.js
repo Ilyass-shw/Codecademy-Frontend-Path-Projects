d99ba663a57b463c0e366a5687c9266e52772343
ilyass shw <souwain.malak@gmail.com>
2021-04-14 15:50:09 +0000

    add test for filterUpdated reducer in postsSlice test

--- Reddit client/my-app/src/features/Posts/postsSlice.test.js
+++ Reddit client/my-app/src/features/Posts/postsSlice.test.js
@@ -1,5 +1,6 @@
 import reducer, { searchTermSet, filterUpdated, firstFilterUpdated, lastFilterUpdated } from "./postsSlice";
 import store from "../../app/store.js";
+
 describe("searchTermSet reducer", () => {
 	it("should handle searchTermSet", () => {
 		let searchTerm = store.getState().posts.searchTerm;
@@ -8,5 +9,24 @@ describe("searchTermSet reducer", () => {
 		store.dispatch(searchTermSet("random"));
 		searchTerm = store.getState().posts.searchTerm;
 		expect(searchTerm).toBe("random");
+
+		store.dispatch(searchTermSet("shiney"));
+		searchTerm = store.getState().posts.searchTerm;
+		expect(searchTerm).toBe("shiney");
+	});
+});
+
+describe("filterUpdated reducer", () => {
+	it("should handle filterUpdated", () => {
+		let filter = store.getState().posts.filter;
+		expect(filter).toBe("relevance");
+
+		store.dispatch(filterUpdated("comments"));
+		filter = store.getState().posts.filter;
+		expect(filter).toBe("comments");
+
+		store.dispatch(filterUpdated("top"));
+		filter = store.getState().posts.filter;
+		expect(filter).toBe("top");
 	});
 });
