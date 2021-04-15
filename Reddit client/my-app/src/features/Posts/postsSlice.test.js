5181f1f461e1b609796849d110d9631bcde5436e
ilyass shw <souwain.malak@gmail.com>
2021-04-15 20:48:55 +0000

    test fetchPost reducer in postsSlice.test.js, as it should change postFetchingStatus from loading to succeeded if the fetching suceedes

--- Reddit client/my-app/src/features/Posts/postsSlice.test.js
+++ Reddit client/my-app/src/features/Posts/postsSlice.test.js
@@ -83,50 +83,50 @@ describe("filterUpdated reducer", () => {
 		});
 	});
 
-	it("should change postFetchingStatus from loading to succeeded if the fetching suceedes", async () => {
-		let postFetchingStatus = store.getState().posts.postFetchingStatus;
-		let postList = store.getState().posts.postList;
-
-		expect(postFetchingStatus).toBe("idle");
-		expect(postList).toHaveLength(0);
-
-		store.dispatch(fetchPosts());
-
-		await wait(() => expect(store.getState().posts.postFetchingStatus).toBe("succeeded"));
-
-		postList = store.getState().posts.postList;
-
-		expect(postList).toHaveLength(3);
-		expect(postList).toMatchSnapshot();
-		await wait(() => cache.clear());
-	});
-
-	// it("should change postFetchingStatus from loading to failed if the fetching fails", async () => {
-	// 	server.use(
-	// 		rest.get("https://www.reddit.com/search.json", (req, res) => {
-	// 			return res((res) => {
-	// 				res.status = 404;
-	// 				res.statusText = "I failed, so sad :(";
-	// 				res.body = JSON.stringify({ error: "You must add request handler." });
-	// 				res.headers.set("Content-Type", "application/json");
-	// 				return res;
-	// 			});
-	// 		})
-	// 	);
-
+	// it("should change postFetchingStatus from loading to succeeded if the fetching suceedes", async () => {
 	// 	let postFetchingStatus = store.getState().posts.postFetchingStatus;
-	// 	let error = store.getState().posts.error;
+	// 	let postList = store.getState().posts.postList;
 
 	// 	expect(postFetchingStatus).toBe("idle");
-	// 	expect(error).toBeNull();
+	// 	expect(postList).toHaveLength(0);
 
 	// 	store.dispatch(fetchPosts());
 
-	// 	await wait(() => expect(store.getState().posts.postFetchingStatus).toBe("failed"));
-	// 	postFetchingStatus = store.getState().posts.postFetchingStatus;
-	// 	error = store.getState().posts.error;
+	// 	await wait(() => expect(store.getState().posts.postFetchingStatus).toBe("succeeded"));
 
-	// 	expect(postFetchingStatus).toBe("failed");
-	// 	expect(error).toBe("I failed, so sad :(");
+	// 	postList = store.getState().posts.postList;
+
+	// 	expect(postList).toHaveLength(3);
+	// 	expect(postList).toMatchSnapshot();
+	// 	await wait(() => cache.clear());
 	// });
+
+	it("should change postFetchingStatus from loading to failed if the fetching fails", async () => {
+		server.use(
+			rest.get("https://www.reddit.com/search.json", (req, res) => {
+				return res((res) => {
+					res.status = 404;
+					res.statusText = "I failed, so sad :(";
+					res.body = JSON.stringify({ error: "You must add request handler." });
+					res.headers.set("Content-Type", "application/json");
+					return res;
+				});
+			})
+		);
+
+		let postFetchingStatus = store.getState().posts.postFetchingStatus;
+		let error = store.getState().posts.error;
+
+		expect(postFetchingStatus).toBe("idle");
+		expect(error).toBeNull();
+
+		store.dispatch(fetchPosts());
+
+		await wait(() => expect(store.getState().posts.postFetchingStatus).toBe("failed"));
+		postFetchingStatus = store.getState().posts.postFetchingStatus;
+		error = store.getState().posts.error;
+
+		expect(postFetchingStatus).toBe("failed");
+		expect(error).toBe("I failed, so sad :(");
+	});
 });
