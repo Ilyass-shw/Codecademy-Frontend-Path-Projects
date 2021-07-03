be2f838c258c62fb7ee4e023be6605215605eaf0
ilyass shw <souwain.malak@gmail.com>
2021-07-03 16:22:38 +0100

    finish test of RegisteredInput

--- shop website/react-readux-typescript/src/components/Checkout/RegisteredInput/RegisteredInput.test.tsx
+++ shop website/react-readux-typescript/src/components/Checkout/RegisteredInput/RegisteredInput.test.tsx
@@ -1,23 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { useForm, FormProvider } from 'react-hook-form';
-import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor } from './helpers';
 import RegisteredInput from './RegisteredInput';
 
 describe('RegisteredInput', () => {
-  interface useFormProps {
-    email: string;
-    phone: number;
-  }
-  const Wrapper: React.FC = ({ children }) => {
-    const methods = useForm<useFormProps>();
-    return (
-      <BrowserRouter>
-        <FormProvider {...methods}>{children}</FormProvider>
-      </BrowserRouter>
-    );
-  };
+  const saveData = jest.fn();
 
   it('should render', () => {
     render(
@@ -30,7 +17,7 @@ describe('RegisteredInput', () => {
         spellCheck="false"
         autoComplete="email"
       />,
-      { wrapper: Wrapper },
+      saveData,
     );
   });
 
@@ -45,17 +32,74 @@ describe('RegisteredInput', () => {
         spellCheck="false"
         autoComplete="email"
       />,
-      { wrapper: Wrapper },
+      saveData,
     );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('should watch input correctly', () => {
-    render(<RegisteredInput Name="phone" type="tel" placeholder="Phone " />, {
-      wrapper: Wrapper,
-    });
-    const registeredInput = screen.getByPlaceholderText('Email');
+    render(
+      <RegisteredInput Name="phone" type="tel" placeholder="Phone " />,
+      saveData,
+    );
+    const registeredInput = screen.getByPlaceholderText('Phone');
     userEvent.type(registeredInput, 'example.net@gmail.com');
     expect(registeredInput).toHaveValue('example.net@gmail.com');
+  });
+
+  it('should display correct error message and submit correctly', async () => {
+    render(
+      <RegisteredInput
+        Name="email"
+        type="email"
+        placeholder="Email"
+        registerOptions={{
+          required: 'This is required.',
+          maxLength: {
+            value: 15,
+            message: 'This phone number is too long.',
+          },
+        }}
+      />,
+      saveData,
+    );
+    const registeredInput = screen.getByPlaceholderText('Email');
+    const submitButton = screen.getByRole('button');
+
+    // verify initial state
+    expect(screen.queryByAltText('This is required.')).not.toBeInTheDocument();
+    expect(
+      screen.queryByAltText('This phone number is too long.'),
+    ).not.toBeInTheDocument();
+
+    // verify state after clicking submit button with empty inputs
+    userEvent.click(submitButton);
+    expect(await screen.findByText('This is required.')).toBeInTheDocument();
+    expect(saveData).not.toHaveBeenCalled();
+
+    //try submitting with an email that is not valid.
+    userEvent.type(registeredInput, 'ThisIsAReallyLongEmailAddress@gmail.com');
+    expect(
+      await screen.findByText('This phone number is too long.'),
+    ).toBeInTheDocument();
+    userEvent.click(submitButton);
+    await waitFor(() => {
+      expect(saveData).not.toHaveBeenCalled();
+    });
+
+    //try an email that is valid.
+    userEvent.clear(registeredInput);
+    userEvent.type(registeredInput, 'ex@gmail.com');
+    await waitFor(() => {
+      expect(
+        screen.queryByText('This phone number is too long.'),
+      ).not.toBeInTheDocument();
+    });
+
+    // submit information
+    userEvent.click(submitButton);
+    await waitFor(() => {
+      expect(saveData).toHaveBeenCalledWith({ email: 'ex@gmail.com' });
+    });
   });
 });
